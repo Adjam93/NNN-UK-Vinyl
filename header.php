11af0a3b8f68d4226c3ca0f5a01f25d5b0858230
Adam <adjam1212@gmail.com>
2024-07-08 19:46:35 +0100

    Updated main font to Poppins

--- header.php
+++ header.php
@@ -10,8 +10,7 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Roboto&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
                  
 </head>
